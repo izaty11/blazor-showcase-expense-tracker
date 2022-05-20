ffa2f6e387866c2752200c14a56d129ca0a007bf
izaty <izaty@DESKTOP-CFQUP43>
2022-05-20 09:51:01 +0800

    text

--- ExpenseTracker.Server/Models/UserInfo.cs
+++ ExpenseTracker.Server/Models/UserInfo.cs
@@ -1,9 +1,12 @@
 ﻿namespace ExpenseTracker.Models
 {
+
     public class UserInfo
     {
         public string FirstName { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
+
+#       public string Email { get; set; }
     }
 }
