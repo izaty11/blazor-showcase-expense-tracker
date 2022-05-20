87a863154e3cc3699e854646db4fa2b731d49c5d
izaty <izaty@DESKTOP-CFQUP43>
2022-05-20 09:52:06 +0800

    eee

--- ExpenseTracker.Server/Models/UserInfo.cs
+++ ExpenseTracker.Server/Models/UserInfo.cs
@@ -7,6 +7,6 @@
         public string FullName { get; set; }
         public string Email { get; set; }
 
-#       public string Email { get; set; }
+       public string Email1 { get; set; }
     }
 }
